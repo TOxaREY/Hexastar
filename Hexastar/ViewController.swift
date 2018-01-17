e3b19cd156512b29567e1e7ae85b34c67e46c8bf
TOxaREY <delta@toxarey.xyz>
2018-01-17 20:52:03 +0300

    code keyboard

--- Hexastar/ViewController.swift
+++ Hexastar/ViewController.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-//        @IBAction func binatrixButton(_ sender: Any) {
-//            let appURL = NSURL(string: "binatrixHexastar://")!
-//            let webURL = NSURL(string: "https://itunes.apple.com/ru/app/binatrix/id1296545616")!
-//            let application = UIApplication.shared
-//
-//            if application.canOpenURL(appURL as URL) {
-//                application.open(appURL as URL)
-//            } else {
-//                application.open(webURL as URL)
-//            }
-//    }
+class ViewController: UIViewController, KeyboardDelegate {
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let keyboardView = Keyboard(frame: CGRect(x: 0, y: 0, width: 0, height: 246))
+        keyboardView.delegate = self
+        textField.inputView = keyboardView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func keyWasTapped(character: String) {
+        if character == "delete" && (textField.text?.isEmpty)! == true {
+            textField.text? = ""
+        } else {
+            if character == "delete" {
+                textField.text?.removeLast()
+            } else {
+                textField.insertText(character)
+            }
+        }
     }
-
-
+    
+    // кнопка перехода на Binatrix
+    //        @IBAction func binatrixButton(_ sender: Any) {
+    //            let appURL = NSURL(string: "binatrixHexastar://")!
+    //            let webURL = NSURL(string: "https://itunes.apple.com/ru/app/binatrix/id1296545616")!
+    //            let application = UIApplication.shared
+    //
+    //            if application.canOpenURL(appURL as URL) {
+    //                application.open(appURL as URL)
+    //            } else {
+    //                application.open(webURL as URL)
+    //            }
+    //    }
+    
+    
 }
 
