8430b3372dc9651260376d729f1243a2819621a4
TOxaREY <delta@toxarey.xyz>
2018-01-20 16:21:23 +0300

    end coding HEXDEC OCTDEC converting

--- HEXDEC.playground/Contents.swift
+++ HEXDEC.playground/Contents.swift
@@ -1,79 +1,142 @@
 import UIKit
 import Foundation
-var myUni = UInt64()
-var multiUni = String()
-myUni = 74
-func hexConv() -> (String) {
-     var hex = String()
-while myUni > 0 {
-        switch myUni % 16 {
-    case 16 : hex.append("10")
-    case 15 : hex.append("F")
-    case 14 : hex.append("E")
-    case 13 : hex.append("D")
-    case 12 : hex.append("C")
-    case 11 : hex.append("B")
-    case 10 : hex.append("A")
-    case 9 : hex.append("9")
-    case 8 : hex.append("8")
-    case 7 : hex.append("7")
-    case 6 : hex.append("6")
-    case 5 : hex.append("5")
-    case 4 : hex.append("4")
-    case 3 : hex.append("3")
-    case 2 : hex.append("2")
-    case 1 : hex.append("1")
-    case 0 : hex.append("0")
+//decimal to hex
+var inputDecDouble = Double()
+inputDecDouble = 1515.55514
+var inputDecInt = UInt64(inputDecDouble)
+var countDecInt = String(inputDecInt).count
+var countFrac = 50 - countDecInt
+var y = 0
+var inputDecFrac = inputDecDouble - Double(inputDecInt)
+var outputHex = String()
+var outputHexFrac = String()
+var outHexTotal = String()
+
+func decHexConv() -> (String) {
+     var hexStringTemp = String()
+while inputDecInt > 0 {
+        switch inputDecInt % 16 {
+    case 16 : hexStringTemp.append("10")
+    case 15 : hexStringTemp.append("F")
+    case 14 : hexStringTemp.append("E")
+    case 13 : hexStringTemp.append("D")
+    case 12 : hexStringTemp.append("C")
+    case 11 : hexStringTemp.append("B")
+    case 10 : hexStringTemp.append("A")
+    case 9 : hexStringTemp.append("9")
+    case 8 : hexStringTemp.append("8")
+    case 7 : hexStringTemp.append("7")
+    case 6 : hexStringTemp.append("6")
+    case 5 : hexStringTemp.append("5")
+    case 4 : hexStringTemp.append("4")
+    case 3 : hexStringTemp.append("3")
+    case 2 : hexStringTemp.append("2")
+    case 1 : hexStringTemp.append("1")
+    case 0 : hexStringTemp.append("0")
 default: break
 }
-        myUni = myUni / 16
+        inputDecInt = inputDecInt / 16
   }
-        let hexRev = Array(hex.reversed())
-        return hexRev.flatMap{String($0)}.joined()
+        let hexStringTempRev = Array(hexStringTemp.reversed())
+        return hexStringTempRev.flatMap{String($0)}.joined()
     }
-if myUni > 0 {
-    multiUni.append(hexConv())
+if inputDecInt > 0 {
+    outputHex.append(decHexConv())
 } else {
-    multiUni = "0"
+    outputHex = "0"
 }
-print(multiUni)
+
+// Frac
+func decHexFracConv() -> (String) {
+   var hexStringFracTemp = String()
+    while inputDecFrac != 0 && y != countFrac {
+        switch UInt64(inputDecFrac * 16) {
+        case 16 : hexStringFracTemp.append("10")
+        case 15 : hexStringFracTemp.append("F")
+        case 14 : hexStringFracTemp.append("E")
+        case 13 : hexStringFracTemp.append("D")
+        case 12 : hexStringFracTemp.append("C")
+        case 11 : hexStringFracTemp.append("B")
+        case 10 : hexStringFracTemp.append("A")
+        case 9 : hexStringFracTemp.append("9")
+        case 8 : hexStringFracTemp.append("8")
+        case 7 : hexStringFracTemp.append("7")
+        case 6 : hexStringFracTemp.append("6")
+        case 5 : hexStringFracTemp.append("5")
+        case 4 : hexStringFracTemp.append("4")
+        case 3 : hexStringFracTemp.append("3")
+        case 2 : hexStringFracTemp.append("2")
+        case 1 : hexStringFracTemp.append("1")
+        case 0 : hexStringFracTemp.append("0")
+        default: break
+        }
+        y += 1
+        inputDecFrac = inputDecFrac * 16 - Double(UInt64(inputDecFrac * 16))
+    }
+    return hexStringFracTemp
+}
+outputHexFrac = decHexFracConv()
+
+outHexTotal = outputHex + "." + outputHexFrac
+//print(outHexTotal)
+
 //hex to decimal
-var hexStr = "111111ABCDEFEDCB"
-func decimalConv() -> (String) {
-var hexArray = [Character]()
-var hexArrayInt = [UInt64]()
-var decimal = [UInt64]()
-for char in hexStr {
-    hexArray.append(char)
+var inputHexString = "FFFFFFFFFFF.FF"
+var outputDec = String()
+
+func hexDecConv() -> (String) {
+var decArrayChar = [Character]()
+var decArrayIntTemp = [UInt64]()
+var decArrayDouble = [Double]()
+var indexString = 0
+var indexDotta = 0
+var z = 0
+for char in inputHexString {
+    indexString += 1
+    if char == "." {
+       indexDotta = indexString
+    }
+    decArrayChar.append(char)
 }
-for i in hexArray {
+for i in decArrayChar {
     switch i {
-    case "F" : hexArrayInt.append(15)
-    case "E" : hexArrayInt.append(14)
-    case "D" : hexArrayInt.append(13)
-    case "C" : hexArrayInt.append(12)
-    case "B" : hexArrayInt.append(11)
-    case "A" : hexArrayInt.append(10)
-    case "9" : hexArrayInt.append(9)
-    case "8" : hexArrayInt.append(8)
-    case "7" : hexArrayInt.append(7)
-    case "6" : hexArrayInt.append(6)
-    case "5" : hexArrayInt.append(5)
-    case "4" : hexArrayInt.append(4)
-    case "3" : hexArrayInt.append(3)
-    case "2" : hexArrayInt.append(2)
-    case "1" : hexArrayInt.append(1)
-    case "0" : hexArrayInt.append(0)
+    case "F" : decArrayIntTemp.append(15)
+    case "E" : decArrayIntTemp.append(14)
+    case "D" : decArrayIntTemp.append(13)
+    case "C" : decArrayIntTemp.append(12)
+    case "B" : decArrayIntTemp.append(11)
+    case "A" : decArrayIntTemp.append(10)
+    case "9" : decArrayIntTemp.append(9)
+    case "8" : decArrayIntTemp.append(8)
+    case "7" : decArrayIntTemp.append(7)
+    case "6" : decArrayIntTemp.append(6)
+    case "5" : decArrayIntTemp.append(5)
+    case "4" : decArrayIntTemp.append(4)
+    case "3" : decArrayIntTemp.append(3)
+    case "2" : decArrayIntTemp.append(2)
+    case "1" : decArrayIntTemp.append(1)
+    case "0" : decArrayIntTemp.append(0)
     default: break
     }
 }
-var decArr = Array(hexArrayInt.reversed())
-for i in 0...decArr.count - 1 {
-    decimal.append(decArr[i] * UInt64(pow(16, Double(i))))
+var decArrayIntTempRev = Array(decArrayIntTemp.reversed())
+    if indexDotta != 0 {
+z = indexDotta - decArrayChar.count
+    } else {
+        z = 0
+    }
+for i in 0...decArrayIntTempRev.count - 1 {
+    decArrayDouble.append(Double(decArrayIntTempRev[i]) * pow(16, Double(z)))
+    z += 1
 }
-    return String(Int(decimal.reduce(0, +)))
+    if indexDotta != 0 {
+        return String(decArrayDouble.reduce(0, +))
+    } else {
+       return String(UInt64(decArrayDouble.reduce(0, +)))
+    }
 }
-print(decimalConv())
+    outputDec = hexDecConv()
 
+print(outputDec)
 
 
--- OCTDEC.playground/Contents.swift
+++ OCTDEC.playground/Contents.swift
@@ -1,63 +1,119 @@
 import UIKit
 import Foundation
-var myUni = UInt64()
-var multiUni = String()
-myUni = 1358469871
-func octConv() -> (String) {
-    var oct = [Int]()
-while myUni > 0 {
-        switch myUni % 8 {
-    case 7 : oct.append(7)
-    case 6 : oct.append(6)
-    case 5 : oct.append(5)
-    case 4 : oct.append(4)
-    case 3 : oct.append(3)
-    case 2 : oct.append(2)
-    case 1 : oct.append(1)
-    case 0 : oct.append(0)
+//decimal to oct
+var inputDecDouble = Double()
+inputDecDouble = 1515.55514
+var inputDecInt = UInt64(inputDecDouble)
+var countDecInt = String(inputDecInt).count
+var countFrac = 50 - countDecInt
+var y = 0
+var inputDecFrac = inputDecDouble - Double(inputDecInt)
+var outputOct = String()
+var outputOctFrac = String()
+var outOctTotal = String()
+
+func decOctConv() -> (String) {
+    var octArrayTemp = [Int]()
+while inputDecInt > 0 {
+        switch inputDecInt % 8 {
+    case 7 : octArrayTemp.append(7)
+    case 6 : octArrayTemp.append(6)
+    case 5 : octArrayTemp.append(5)
+    case 4 : octArrayTemp.append(4)
+    case 3 : octArrayTemp.append(3)
+    case 2 : octArrayTemp.append(2)
+    case 1 : octArrayTemp.append(1)
+    case 0 : octArrayTemp.append(0)
 default: break
 }
-        myUni = myUni / 8
+        inputDecInt = inputDecInt / 8
   }
-        let octRev = Array(oct.reversed())
-        return octRev.flatMap{String($0)}.joined()
+        let octStringTempRev = Array(octArrayTemp.reversed())
+        return octStringTempRev.flatMap{String($0)}.joined()
     }
-if myUni > 0 {
-    multiUni.append(octConv())
+if inputDecInt > 0 {
+    outputOct.append(decOctConv())
 } else {
-    multiUni = "0"
+    outputOct = "0"
 }
-print(multiUni)
+
+// Frag
+func decOctFracConv() -> (String) {
+     var octArrayFracTemp = [Int]()
+     while inputDecFrac != 0 && y != countFrac {
+          switch UInt64(inputDecFrac * 8) {
+          case 7 : octArrayFracTemp.append(7)
+          case 6 : octArrayFracTemp.append(6)
+          case 5 : octArrayFracTemp.append(5)
+          case 4 : octArrayFracTemp.append(4)
+          case 3 : octArrayFracTemp.append(3)
+          case 2 : octArrayFracTemp.append(2)
+          case 1 : octArrayFracTemp.append(1)
+          case 0 : octArrayFracTemp.append(0)
+          default: break
+    }
+          y += 1
+          inputDecFrac = inputDecFrac * 8 - Double(UInt64(inputDecFrac * 8))
+    }
+    return octArrayFracTemp.flatMap{String($0)}.joined()
+}
+outputOctFrac = decOctFracConv()
+
+outOctTotal = outputOct + "." + outputOctFrac
+//print(outOctTotal)
+
+
 //oct to decimal
-var octStr = "17745625310"
-func decimalConv() -> (String) {
-var octArray = [Character]()
-var octArrayInt = [UInt64]()
-var decimal = [UInt64]()
-for char in octStr {
-    octArray.append(char)
+var inputOctString = "177456.25310"
+var outputDec = String()
+
+func octDecConv() -> (String) {
+var decArrayChar = [Character]()
+var decArrayIntTemp = [UInt64]()
+var decArrayDouble = [Double]()
+var indexString = 0
+var indexDotta = 0
+var z = 0
+for char in inputOctString {
+    indexString += 1
+    if char == "." {
+        indexDotta = indexString
+    }
+    decArrayChar.append(char)
 }
-    for i in octArray {
+    for i in decArrayChar {
         switch i {
-        case "7" : octArrayInt.append(7)
-        case "6" : octArrayInt.append(6)
-        case "5" : octArrayInt.append(5)
-        case "4" : octArrayInt.append(4)
-        case "3" : octArrayInt.append(3)
-        case "2" : octArrayInt.append(2)
-        case "1" : octArrayInt.append(1)
-        case "0" : octArrayInt.append(0)
+        case "7" : decArrayIntTemp.append(7)
+        case "6" : decArrayIntTemp.append(6)
+        case "5" : decArrayIntTemp.append(5)
+        case "4" : decArrayIntTemp.append(4)
+        case "3" : decArrayIntTemp.append(3)
+        case "2" : decArrayIntTemp.append(2)
+        case "1" : decArrayIntTemp.append(1)
+        case "0" : decArrayIntTemp.append(0)
         default: break
         }
     }
 
-var decArr = Array(octArrayInt.reversed())
-for i in 0...decArr.count - 1 {
-    decimal.append(decArr[i] * UInt64(pow(8, Double(i))))
+var decArrayIntTempRev = Array(decArrayIntTemp.reversed())
+    if indexDotta != 0 {
+        z = indexDotta - decArrayChar.count
+    } else {
+        z = 0
+    }
+for i in 0...decArrayIntTempRev.count - 1 {
+    decArrayDouble.append(Double(decArrayIntTempRev[i]) * pow(8, Double(z)))
+    z += 1
 }
-    return String(Int(decimal.reduce(0, +)))
+    if indexDotta != 0 {
+        return String(decArrayDouble.reduce(0, +))
+    } else {
+        return String(UInt64(decArrayDouble.reduce(0, +)))
+    }
 }
-print(decimalConv())
+
+outputDec = octDecConv()
+print(outputDec)
 
 
 
