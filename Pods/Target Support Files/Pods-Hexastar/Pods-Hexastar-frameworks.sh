5c44313ab1402c9b303bfa20b1143c961ea0edf9
TOxaREY <delta@toxarey.xyz>
2020-09-15 18:54:37 +0300

    pod update

--- Pods/Target Support Files/Pods-Hexastar/Pods-Hexastar-frameworks.sh
+++ Pods/Target Support Files/Pods-Hexastar/Pods-Hexastar-frameworks.sh
@@ -197,11 +197,8 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${PODS_ROOT}/YandexMobileMetrica/dynamic/YandexMobileMetrica.framework"
   install_framework "${PODS_ROOT}/YandexMobileMetrica/dynamic/YandexMobileMetricaCrashes.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
